dee7be0e4a2c4526d87087f7f7d68f87400c2160
ElleryQu <844390495@qq.com>
2025-09-26 09:42:11 +0000

    [upd] prepare for artifact check

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt install libgtest-dev libssl-dev python3 python3-pip
+git submodule update --init --recursive
+
+# please fill in the location of the nvcc compiler based on your CUDA toolkit installation status. 
+# specifically, modify the `CUDA_VERSION` variables under `files/make/Makefile-*`.
+NEW_CUDA_VERSION=$(nvcc --version | grep -oP '(?<=release )\d+\.\d+')
+sed -i "s/^CUDA_VERSION=.*/CUDA_VERSION=$NEW_CUDA_VERSION/" files/make/Makefile-PFalcon
+sed -i "s/^CUDA_VERSION=.*/CUDA_VERSION=$NEW_CUDA_VERSION/" files/make/Makefile-Mizar
+sed -i "s/^CUDA_VERSION=.*/CUDA_VERSION=$NEW_CUDA_VERSION/" files/make/Makefile-Mizar
+
+# build pfalcon, aegis and mizar.
+./scripts/quick_make.sh
+
+# all the binary files will be installed under `./output/bin/`.

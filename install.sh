024c23fa618af6b78ac2d8a1f651a860a11db83e
ElleryQu <844390495@qq.com>
2025-09-27 13:22:31 +0000

    [upd] update install.sh for tips

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 apt install libgtest-dev libssl-dev python3 python3-pip
 git submodule update --init --recursive
 
-# please fill in the location of the nvcc compiler based on your CUDA toolkit installation status. 
-# specifically, modify the `CUDA_VERSION` variables under `files/make/Makefile-*`.
+# We need to locate the NVCC compiler binary and populate its path into the 
+# Makefile under files/make. An automated method is provided here to detect 
+# the NVCC compiler location. If this method fails, users can manually 
+# locate the NVCC compiler using the whereis nvcc command.
 sed -i "9s|.*|CXX=$(which nvcc)|" files/make/Makefile-PFalcon
 sed -i "9s|.*|CXX=$(which nvcc)|" files/make/Makefile-Aegis
 sed -i "9s|.*|CXX=$(which nvcc)|" files/make/Makefile-Mizar
